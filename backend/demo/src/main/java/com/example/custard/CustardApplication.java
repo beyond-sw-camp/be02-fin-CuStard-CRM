c917b32bd69cbd8f38ffefcd4b71d887445d3c7d
baeksongyeon <songyeon0607@naver.com>
2024-03-02 01:14:27 +0900

    [Feat] 프로젝트 구조 추가 - Pom.xml, application.yml, CustardApplication.java 추가

---
+++ backend/demo/src/main/java/com/example/custard/CustardApplication.java
@@ -0,0 +1,13 @@
+package com.example.custard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustardApplication.class, args);
+	}
+
+}
