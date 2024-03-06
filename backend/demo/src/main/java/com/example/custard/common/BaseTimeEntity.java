a66fb795c7796457bb4054a090ffdfed5c8b75ed
baeksongyeon <songyeon0607@naver.com>
2024-03-06 14:40:05 +0900

    [Fix] BaseEntity 내용 추가 - 데이터가 생성되는 시간을 저장하는 추상 클래스 내용 추가

--- backend/demo/src/main/java/com/example/custard/common/BaseTimeEntity.java
+++ backend/demo/src/main/java/com/example/custard/common/BaseTimeEntity.java
@@ -1,4 +1,18 @@
 package com.example.custard.common;
 
-public class BaseTimeEntity {
+import lombok.Getter;
+import org.hibernate.annotations.Entity;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createdDate;
 }
