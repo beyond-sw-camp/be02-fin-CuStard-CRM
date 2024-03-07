405cf3b02c1d8b805518b1339c9a1308dfe21f45
baeksongyeon <songyeon0607@naver.com>
2024-03-07 11:32:34 +0900

    [Feat] 주문/결제 기능 구현(#8, #9) - 주문 기능 추가 - 아임포트를 이용한 카카오페이 결제 기능 추가

---
+++ backend/src/main/java/com/example/backend/orders/controller/OrdersController.java
@@ -0,0 +1,31 @@
+package com.example.backend.orders.controller;
+
+import com.example.backend.orders.model.response.GetOrdersCreateRes;
+import com.example.backend.orders.service.OrdersService;
+import com.example.backend.orders.service.PaymentService;
+import com.siot.IamportRestClient.exception.IamportResponseException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/orders")
+@CrossOrigin("*")
+public class OrdersController {
+    private final OrdersService ordersService;
+    private final PaymentService paymentService;
+
+    @RequestMapping(method = RequestMethod.GET,value = "/validation")
+    public ResponseEntity<GetOrdersCreateRes> ordersCreate(@RequestHeader(value = "Authorization") String token, String impUid) throws IamportResponseException, IOException {
+            //TODO: 프론트 연결 후 유효성 검증 가능(주석 해제)
+//            if(paymentService.paymentValidation(impUid)){
+//                return ordersService.createOrder(token, impUid);
+//            }
+//            return null;
+
+        return ordersService.createOrder(token, impUid);
+
+    }
+}
---
+++ backend/src/main/java/com/example/backend/orders/repository/OrdersRepository.java
@@ -0,0 +1,10 @@
+package com.example.backend.orders.repository;
+
+
+import com.example.backend.orders.model.entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+}
