ea4d6f514eaf3b4892dc9f8cecb7cc43e436f6f6
Hyeon28 <96675421+Hyeon28@users.noreply.github.com>
2024-03-08 12:36:31 +0900

    [Chore] backend_admin 프로젝트 생성

---
+++ backend_admin/src/main/java/com/example/backend_admin/BackendAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAdminApplication.class, args);
+	}
+
+}
