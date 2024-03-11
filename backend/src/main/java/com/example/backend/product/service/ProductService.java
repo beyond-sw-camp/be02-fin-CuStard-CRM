fd84caf5f94e36d6b49006c4281c9fced9944e3c
Hyeon28 <96675421+Hyeon28@users.noreply.github.com>
2024-03-11 16:01:36 +0900

    [Feat] 추천 상품 기능 구현 (#14)
    등록된 물건들 / 조회, 구매한 물건들을 기반으로 아이템 유사도 계산 후 추천 상품 리스트 반환

--- backend/src/main/java/com/example/backend/product/service/ProductService.java
+++ backend/src/main/java/com/example/backend/product/service/ProductService.java
@@ -3,19 +3,23 @@ package com.example.backend.product.service;
 
 import com.example.backend.customer.model.entity.Customer;
 import com.example.backend.customer.repository.CustomerRepository;
+import com.example.backend.log.entity.ProductDetailLog;
+import com.example.backend.log.repository.ProductDetailLogRespository;
 import com.example.backend.log.service.ProductDetailLogService;
 import com.example.backend.log.service.SearchLogService;
+import com.example.backend.orders.model.entity.Orders;
+import com.example.backend.orders.repository.OrdersRepository;
 import com.example.backend.product.model.entity.Product;
 import com.example.backend.product.model.response.GetProductListRes;
+import com.example.backend.product.model.response.GetProductRecRes;
 import com.example.backend.product.model.response.GetProductRes;
 import com.example.backend.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,12 +28,14 @@ public class ProductService {
     private final ProductDetailLogService productDetailLogService;
     private final CustomerRepository customerRepository;
     private final SearchLogService searchLogService;
+    private final ProductDetailLogRespository productDetailLogRespository;
+    private final OrdersRepository ordersRepository;
 
     public List<GetProductListRes> list() {
         List<GetProductListRes> productListRes = new ArrayList<>();
 
-        List<Product> productList= productRepository.findAll();
-        for(Product product : productList){
+        List<Product> productList = productRepository.findAll();
+        for (Product product : productList) {
             productListRes.add(GetProductListRes.builder()
                     .idx(product.getIdx())
                     .category(product.getCategory())
@@ -44,7 +50,7 @@ public class ProductService {
     public GetProductRes read(Long idx, Authentication authentication) {
         Optional<Product> result = productRepository.findById(idx);
 
-        if(result.isPresent()) {
+        if (result.isPresent()) {
             Product product = result.get();
             GetProductRes getProductRes = GetProductRes.builder()
                     .idx(product.getIdx())
@@ -54,10 +60,10 @@ public class ProductService {
                     .productPrice(product.getProductPrice())
                     .build();
 
-            if(authentication != null && authentication.isAuthenticated()) {
-                Customer customer = (Customer)authentication.getPrincipal();
+            if (authentication != null && authentication.isAuthenticated()) {
+                Customer customer = (Customer) authentication.getPrincipal();
                 Optional<Customer> c = customerRepository.findById(customer.getIdx());
-                if(c.isPresent()) {
+                if (c.isPresent()) {
                     productDetailLogService.productDetailLogging(c.get(), product);
                 }
             }
@@ -71,7 +77,7 @@ public class ProductService {
 
         List<GetProductListRes> productListRes = new ArrayList<>();
 
-        for(Product product : productList){
+        for (Product product : productList) {
             productListRes.add(GetProductListRes.builder()
                     .idx(product.getIdx())
                     .category(product.getCategory())
@@ -80,13 +86,199 @@ public class ProductService {
                     .build());
         }
 
-        if(authentication != null && authentication.isAuthenticated()) {
-            Customer customer = (Customer)authentication.getPrincipal();
+        if (authentication != null && authentication.isAuthenticated()) {
+            Customer customer = (Customer) authentication.getPrincipal();
             Optional<Customer> c = customerRepository.findById(customer.getIdx());
-            if(c.isPresent()) {
+            if (c.isPresent()) {
                 searchLogService.SearchLogging(customer, keyword);
             }
         }
         return productListRes;
+    }
+
+    public GetProductRecRes recommend() {
+        // 아이템 간의 유사도를 계산하기 위한 아이템 특성 정보
+        Map<String, Map<String, Double>> productFeatures = new HashMap<>();
+        // 예시 아이템의 특성 정보 추가
+        List<Product> products = productRepository.findAll();
+
+
+        for (Product product : products) {
+            Map<String, Double> productFeaturesMap = new HashMap<>();
+            productFeaturesMap.put(product.getCategory().toString(), 1.0);
+            productFeaturesMap.put(product.getProductName(), 1.0);
+            productFeaturesMap.put(product.getProductDetails(), 1.0);
+
+            productFeatures.put(product.getProductName(), productFeaturesMap);
+        }
+
+        // 아이템 간의 유사도 계산
+        Map<String, Map<String, Double>> similarityMatrix = calculateProductSimilarity(productFeatures);
+
+        // 테스트 사용자와 그 사용자의 조회 및 구매 내역
+        Map<Customer, List<Product>> viewHistory = new HashMap<>();
+        List<ProductDetailLog> productDetailLogs = productDetailLogRespository.findAll();
+        for (ProductDetailLog productDetailLog : productDetailLogs) {
+            Customer customer = productDetailLog.getCustomer();
+            Product product = productDetailLog.getProduct();
+            if (!viewHistory.containsKey(customer)) {
+                viewHistory.put(customer, new ArrayList<>());
+            }
+            viewHistory.get(customer).add(product);
+        }
+        Map<Customer, List<Product>> purchaseHistory = new HashMap<>();
+        List<Orders> orders = ordersRepository.findAll();
+        for (Orders order : orders) {
+            Customer customer = order.getCustomer();
+            Optional<Product> result = productRepository.findById(order.getProductIdx());
+            Product product = result.get();
+
+            if (!purchaseHistory.containsKey(customer)) {
+                purchaseHistory.put(customer, new ArrayList<>());
+            }
+            purchaseHistory.get(customer).add(product);
+        }
+
+        // 사용자들에게 추천될 아이템과 점수 계산
+        Map<Customer, Map<Product, Double>> recommendationScores = recommendProducts(similarityMatrix, viewHistory, purchaseHistory);
+
+        Map<String, Map<String, Double>> recommendList = new HashMap<>();
+        for (Map.Entry<Customer, Map<Product, Double>> entry : recommendationScores.entrySet()) {
+            Customer customer = entry.getKey();
+            Map<Product, Double> productScores = entry.getValue();
+            Map<String, Double> productList = new HashMap<>();
+            for (Map.Entry<Product, Double> productEntry : productScores.entrySet()) {
+                Product product = productEntry.getKey();
+                Double score = productEntry.getValue();
+                String productName = product.getProductName();
+                productList.put(productName, score);
+            }
+            recommendList.put(customer.getCustomerEmail(), productList);
+        }
+        return GetProductRecRes.builder().recommendList(recommendList).build();
+
+    }
+//    public Map<String, Map<String, Double>> calculateProductSimilarity(Map<String, Map<String, Double>> productFeatures) {
+//        Map<String, Map<String, Double>> similarityMatrix = new HashMap<>();
+//
+//        for (Map.Entry<String, Map<String, Double>> entry1 : productFeatures.entrySet()) {
+//            String product1 = entry1.getKey();
+//            Map<String, Double> features1 = entry1.getValue();
+//
+//            // 아이템1과 다른 모든 아이템 간의 유사도 계산
+//            Map<String, Double> similarities = new HashMap<>();
+//            for (Map.Entry<String, Map<String, Double>> entry2 : productFeatures.entrySet()) {
+//                String product2 = entry2.getKey();
+//                if (!product1.equals(product2)) {
+//                    Map<String, Double> features2 = entry2.getValue();
+//                    double similarity = calculateJaccardSimilarity(features1.keySet(), features2.keySet());
+//                    similarities.put(product2, similarity);
+//                }
+//            }
+//            similarityMatrix.put(product1, similarities);
+//        }
+//
+//        return similarityMatrix;
+//    }
+//
+//    // 자카드 유사도 계산
+//    private double calculateJaccardSimilarity(Set<String> set1, Set<String> set2) {
+//        Set<String> intersection = new HashSet<>(set1);
+//        intersection.retainAll(set2);
+//
+//        Set<String> union = new HashSet<>(set1);
+//        union.addAll(set2);
+//
+//        if (union.size() == 0) {
+//            return 0.0;
+//        } else {
+//            return (double) intersection.size() / union.size();
+//        }
+//    }
+
+    public Map<String, Map<String, Double>> calculateProductSimilarity(Map<String, Map<String, Double>> productFeatures) {
+        Map<String, Map<String, Double>> similarityMatrix = new HashMap<>();
+
+        for (Map.Entry<String, Map<String, Double>> entry1 : productFeatures.entrySet()) {
+            String product1 = entry1.getKey();
+            Map<String, Double> features1 = entry1.getValue();
+
+            // 아이템1과 다른 모든 아이템 간의 유사도 계산
+            Map<String, Double> similarities = new HashMap<>();
+            for (Map.Entry<String, Map<String, Double>> entry2 : productFeatures.entrySet()) {
+                String product2 = entry2.getKey();
+                if (!product1.equals(product2)) {
+                    Map<String, Double> features2 = entry2.getValue();
+                    double similarity = calculateCosineSimilarity(features1, features2);
+                    similarities.put(product2, similarity);
+                }
+            }
+            similarityMatrix.put(product1, similarities);
+        }
+
+        return similarityMatrix;
+    }
+
+    // 코사인 유사도 계산
+    private double calculateCosineSimilarity(Map<String, Double> vector1, Map<String, Double> vector2) {
+        double dotProduct = 0;
+        double magnitude1 = 0;
+        double magnitude2 = 0;
+
+        for (Map.Entry<String, Double> entry : vector1.entrySet()) {
+            String feature = entry.getKey();
+            double value1 = entry.getValue();
+            double value2 = vector2.getOrDefault(feature, 0.0);
+
+            dotProduct += value1 * value2;
+            magnitude1 += Math.pow(value1, 2);
+            magnitude2 += Math.pow(value2, 2);
+        }
+
+        double magnitudeProduct = Math.sqrt(magnitude1) * Math.sqrt(magnitude2);
+        if (magnitudeProduct == 0) {
+            return 0; // 분모가 0이 되는 경우 예외 처리
+        } else {
+            return dotProduct / magnitudeProduct;
+        }
+    }
+
+    public Map<Customer, Map<Product, Double>> recommendProducts(Map<String, Map<String, Double>> similarityMatrix, Map<Customer, List<Product>> viewHistory, Map<Customer, List<Product>> purchaseHistory) {
+        Map<Customer, Map<Product, Double>> recommendationScores = new HashMap<>();
+
+        List<Customer> customers = customerRepository.findAll();
+        for (Customer customer : customers) {
+            Set<Product> viewedProducts = new HashSet<>(viewHistory.getOrDefault(customer, Collections.emptyList()));
+            Set<Product> purchasedProducts = new HashSet<>(purchaseHistory.getOrDefault(customer, Collections.emptyList()));
+
+            Map<Product, Double> recommendationScoresMap = new HashMap<>();
+            for (Customer otherCustomer : customers) {
+                if (!otherCustomer.equals(customer)) {
+                    List<Product> otherPurchasedProducts = purchaseHistory.getOrDefault(otherCustomer, Collections.emptyList());
+                    for (Product otherPurchasedProduct : otherPurchasedProducts) {
+                        double similarity = calculateProductSimilarity(similarityMatrix, otherPurchasedProduct.getProductName(), viewedProducts);
+                        if (similarity > 0 && !purchasedProducts.contains(otherPurchasedProduct)) {
+                            recommendationScoresMap.put(otherPurchasedProduct, recommendationScoresMap.getOrDefault(otherPurchasedProduct, 0.0) + similarity);
+                        }
+                    }
+                }
+            }
+
+            Map<Product, Double> sortedRecommendationScoresMap = recommendationScoresMap.entrySet().stream()
+                    .sorted(Map.Entry.<Product, Double>comparingByValue().reversed())
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+
+            recommendationScores.put(customer, sortedRecommendationScoresMap);
+        }
+
+        return recommendationScores;
+    }
+
+    private double calculateProductSimilarity(Map<String, Map<String, Double>> similarityMatrix, String viewedProductName, Set<Product> purchasedProducts) {
+        double similarityScore = 0.0;
+        for (Product purchasedProduct : purchasedProducts) {
+            similarityScore += similarityMatrix.getOrDefault(purchasedProduct.getProductName(), Collections.emptyMap()).getOrDefault(viewedProductName, 0.0);
+        }
+        return similarityScore;
     }
 }
