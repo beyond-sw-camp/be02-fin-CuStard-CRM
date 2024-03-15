70fd51892bc56f9dfde944eb553e5fd65f9e5ea9
JungDongSeob <ehdtjqzz@gmail.com>
2024-03-16 00:59:41 +0900

    [Fix] 메인 페이지 집계 함수 수정 (#113) - 휴면 고객 집계 함수 수정

--- backend_admin/src/main/java/com/example/backend_admin/calculate/service/CalculateLogService.java
+++ backend_admin/src/main/java/com/example/backend_admin/calculate/service/CalculateLogService.java
@@ -42,9 +42,14 @@ public class CalculateLogService {
     }
 
     public GetSleepAccountGrowthRateRes sleepAccountGrowthRate(){
-        double sleepAccountGrowthRate = ((customerRepository.count() - loginLogRespository.findByOneDaysAgoActiveCount()) - (customerRepository.count() - loginLogRespository.findByTwoDaysAgoActiveCount()))/(double)loginLogRespository.findByTwoDaysAgoActiveCount()*100;
-        System.out.println("최종 : " + loginLogRespository.findByOneDaysAgoActiveCount());
-        System.out.println("초기 : " + loginLogRespository.findByTwoDaysAgoActiveCount());
+        double sleepAccountGrowthRate = (((loginLogRespository.findByAcountCountToday()-loginLogRespository.findByTodaysAgoActiveCount())-(loginLogRespository.findByAcountCountDayAgo()-loginLogRespository.findByOneDaysAgoActiveCount()))/(double)(loginLogRespository.findByAcountCountDayAgo()-loginLogRespository.findByOneDaysAgoActiveCount()))*100;
+//        System.out.println("현재 : " + (loginLogRespository.findByAcountCountToday()-loginLogRespository.findByTodaysAgoActiveCount()));
+//        System.out.println("이전 : " + (loginLogRespository.findByAcountCountDayAgo()-loginLogRespository.findByOneDaysAgoActiveCount()));
+//        System.out.println(loginLogRespository.findByAcountCountToday());
+//        System.out.println(loginLogRespository.findByTodaysAgoActiveCount());
+//        System.out.println(loginLogRespository.findByAcountCountDayAgo());
+//        System.out.println(loginLogRespository.findByOneDaysAgoActiveCount());
+        System.out.println(sleepAccountGrowthRate);
         sleepAccountGrowthRate = Math.round(sleepAccountGrowthRate*100)/100.0;
 
         return GetSleepAccountGrowthRateRes.builder()
