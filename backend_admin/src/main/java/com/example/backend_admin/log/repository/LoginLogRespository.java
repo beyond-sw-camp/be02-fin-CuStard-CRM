b4a102ec563ad2b355e4a0d19aa5599f337be279
baeksongyeon <songyeon0607@naver.com>
2024-03-15 09:41:02 +0900

    [Feat] 메인 대시보드 방문자 수와 전날 대비 방문자 수 집계 기능 구현(#107)

--- backend_admin/src/main/java/com/example/backend_admin/log/repository/LoginLogRespository.java
+++ backend_admin/src/main/java/com/example/backend_admin/log/repository/LoginLogRespository.java
@@ -1,14 +1,18 @@
 package com.example.backend_admin.log.repository;
 
 
+import com.example.backend_admin.calculate.model.response.GetTodayLoginRes;
 import com.example.backend_admin.log.entity.LoginLog;
 import com.example.backend_admin.orders.model.entity.Orders;
 import com.mysql.cj.log.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface LoginLogRespository extends JpaRepository<LoginLog, Long> {
     List<LoginLog> findByCustomerIdx(Long customerIdx);
+
+    Long countByCreatedDateAfter(LocalDateTime createdDate);
 }
