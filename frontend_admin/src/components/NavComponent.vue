8c9c627c3e08d84af8cf5df4cd6c4e3030e45192
kangmoonhye <ansgp0323@naver.com>
2024-03-18 17:17:09 +0900

    [Feat] 고객 대시보드 수정 (#93)

--- frontend_admin/src/components/NavComponent.vue
+++ frontend_admin/src/components/NavComponent.vue
@@ -4,8 +4,7 @@
         class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center"
     >
       <a class="navbar-brand brand-logo-mini" href="/main"
-      ><img src="assets/images/logo-mini.svg" alt="logo"
-      /></a>
+      ></a>
     </div>
     <div class="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
       <button
