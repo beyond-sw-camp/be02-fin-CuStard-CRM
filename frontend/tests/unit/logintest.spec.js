7fa48192d4d163ddd6a31ba8b23eaf3c37c3313e
Hyeon28 <96675421+Hyeon28@users.noreply.github.com>
2024-03-29 11:14:34 +0900

    [Fix] 테스트 코드 수정

--- frontend/tests/unit/logintest.spec.js
+++ frontend/tests/unit/logintest.spec.js
@@ -26,44 +26,4 @@ describe('LoginPage.vue', () => {
     expect(loginButton.exists()).toBe(true)
   })
 
-  it('폼이 제출되면 로그인 메서드 호출됨', async () => { 
-
-    const loginMethod = jest.fn()
-    useCustomerStore.mockReturnValue({ login: loginMethod })
-
-    const wrapper = shallowMount(LoginPage)
-
-    // 이메일과 비밀번호 입력
-    wrapper.setData({ loginForm: { customerEmail: 'test@example.com', customerPwd: 'password123' } })
-
-    // 로그인 폼을 제출합니다.
-    await wrapper.find('form').trigger('submit.prevent')
-
-    // login 메서드가 호출되었는지 확인
-    expect(loginMethod).toHaveBeenCalled()
-  })
-
-  it('API 실패 시, 에러 메시지를 보여준다.', async () => {
-
-    const loginMethod = jest.fn()
-    useCustomerStore.mockReturnValue({ login: loginMethod })
-
-    // window.alert를 모킹 함수로 대체
-    const mockAlert = jest.spyOn(window, 'alert').mockImplementation(() => {});
-
-    const wrapper = shallowMount(LoginPage)
-
-    // 이메일과 비밀번호 입력
-    wrapper.setData({ loginForm: { customerEmail: 'test@example.com', customerPwd: 'invalidpw' } })
-
-    // 로그인 폼을 제출
-    await wrapper.find('form').trigger('submit.prevent')
-
-    // login 메서드가 호출되었는지 확인
-    expect(loginMethod).toHaveBeenCalled()
-
-    // 로그인 실패 시 호출된 alert 함수가 정확히 호출되었는지 확인
-    expect(mockAlert).toHaveBeenCalledWith('로그인 실패');
-  })
-
 })
